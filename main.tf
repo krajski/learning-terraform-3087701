fc7ffe138c6f2c376e2de082d5c81c2733e174b0
krajski <krystian.rajski@intel.com>
2024-01-08 16:50:54 +0100

    Craete security group

--- main.tf
+++ main.tf
@@ -31,9 +31,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name = "blog"
-  tags = {
-    Terraform = "true"
-  }
+  description = "Allow HTTP and HTTPS in. Allow everything out."
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -46,7 +44,6 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
@@ -55,7 +52,6 @@ resource "aws_security_group_rule" "blog_https_in" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
-
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
